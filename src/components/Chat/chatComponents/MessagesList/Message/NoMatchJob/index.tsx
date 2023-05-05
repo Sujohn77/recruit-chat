5e4d152ca40f8cd0f7158e43950f8054b3b38c05
Artem-Pavlenko <rozen19942810@gmail.com>
2023-05-05 13:30:34 +0300

    [AP]refactor: no match job message

--- src/components/Chat/chatComponents/MessagesList/Message/NoMatchJob/index.tsx
+++ src/components/Chat/chatComponents/MessagesList/Message/NoMatchJob/index.tsx
@@ -1,54 +1,9 @@
 import { useChatMessenger } from "contexts/MessengerContext";
 import { FC } from "react";
-import { Button } from "@mui/material";
 import { useTranslation } from "react-i18next";
-import styled from "styled-components";
 
-import { colors } from "utils/colors";
 import { CHAT_ACTIONS } from "utils/types";
-
-export const Wrapper = styled.div<{ isRefineOnly?: boolean }>`
-  background: ${({ theme: { message } }) => message.backgroundColor};
-  border-radius: 10px;
-  padding: 24px 28px;
-  margin: ${({ isRefineOnly }) => !isRefineOnly && "0 16px"};
-  display: flex;
-  flex-flow: column;
-  max-width: 306px;
-  box-sizing: border-box;
-  margin-bottom: 24px;
-`;
-
-export const Title = styled.p`
-  margin: 0 0 24px;
-  font-size: 14px;
-  line-height: 17px;
-  color: ${({ theme: { text } }) => text.color};
-  text-align: center;
-`;
-
-export const Avatar = styled.div`
-  width: 69px;
-  height: 69px;
-  border-radius: 50%;
-  margin: 0 auto 32px;
-  background-color: ${colors.white};
-`;
-
-export const SetJobAlert = styled(Button)`
-  margin: 0 0 16px !important;
-  color: ${(props) => props.theme.primaryColor}!important;
-  border: 1px solid ${(props) => props.theme.primaryColor}!important;
-  border-radius: 100px !important;
-  text-transform: initial !important;
-`;
-
-export const RefineJobSearch = styled(Button)`
-  background-color: ${(props) => props.theme.primaryColor}!important;
-  border-radius: 100px !important;
-  color: ${({ theme: { button } }) => button.secondaryColor}!important;
-  text-transform: initial !important;
-`;
+import * as S from "./styles";
 
 interface INoMatchJobProps {
   isRefineOnly?: boolean;
@@ -60,33 +15,33 @@ export const NoMatchJob: FC<INoMatchJobProps> = ({ isRefineOnly = false }) => {
 
   if (isRefineOnly) {
     return (
-      <Wrapper isRefineOnly>
-        <Avatar />
-        <RefineJobSearch
+      <S.Wrapper isRefineOnly>
+        <S.Avatar />
+        <S.RefineJobSearch
           onClick={() => triggerAction({ type: CHAT_ACTIONS.REFINE_SEARCH })}
         >
           {t("buttons:refine_search")}
-        </RefineJobSearch>
-      </Wrapper>
+        </S.RefineJobSearch>
+      </S.Wrapper>
     );
   }
 
   return (
-    <Wrapper>
-      <Title>{t("chat_item_description:no_match")}</Title>
-      <Avatar />
+    <S.Wrapper>
+      <S.Title>{t("chat_item_description:no_match")}</S.Title>
+      <S.Avatar />
 
-      <SetJobAlert
+      <S.SetJobAlert
         onClick={() => triggerAction({ type: CHAT_ACTIONS.SET_JOB_ALERT })}
       >
         {t("buttons:set_job_alert")}
-      </SetJobAlert>
+      </S.SetJobAlert>
 
-      <RefineJobSearch
+      <S.RefineJobSearch
         onClick={() => triggerAction({ type: CHAT_ACTIONS.REFINE_SEARCH })}
       >
         {t("buttons:refine_search")}
-      </RefineJobSearch>
-    </Wrapper>
+      </S.RefineJobSearch>
+    </S.Wrapper>
   );
 };
---
+++ src/components/Chat/chatComponents/MessagesList/Message/NoMatchJob/styles.ts
@@ -0,0 +1,46 @@
+import { Button } from "@mui/material";
+import styled from "styled-components";
+import { colors } from "utils/colors";
+
+export const Wrapper = styled.div<{ isRefineOnly?: boolean }>`
+  background: ${({ theme: { message } }) => message.backgroundColor};
+  border-radius: 10px;
+  padding: 24px 28px;
+  margin: ${({ isRefineOnly }) => !isRefineOnly && "0 16px"};
+  display: flex;
+  flex-flow: column;
+  max-width: 306px;
+  box-sizing: border-box;
+  margin-bottom: 24px;
+`;
+
+export const Title = styled.p`
+  margin: 0 0 24px;
+  font-size: 14px;
+  line-height: 17px;
+  color: ${({ theme: { text } }) => text.color};
+  text-align: center;
+`;
+
+export const Avatar = styled.div`
+  width: 69px;
+  height: 69px;
+  border-radius: 50%;
+  margin: 0 auto 32px;
+  background-color: ${colors.white};
+`;
+
+export const SetJobAlert = styled(Button)`
+  margin: 0 0 16px !important;
+  color: ${(props) => props.theme.primaryColor}!important;
+  border: 1px solid ${(props) => props.theme.primaryColor}!important;
+  border-radius: 100px !important;
+  text-transform: initial !important;
+`;
+
+export const RefineJobSearch = styled(Button)`
+  background-color: ${(props) => props.theme.primaryColor}!important;
+  border-radius: 100px !important;
+  color: ${({ theme: { button } }) => button.secondaryColor}!important;
+  text-transform: initial !important;
+`;
