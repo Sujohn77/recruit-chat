05c8001b54ba0fe5bbb4d2a51c4fa703d976f8e8
Artem-Pavlenko <rozen19942810@gmail.com>
2023-10-04 14:23:04 +0300

    [AP]refactor: locations mess

--- src/components/Chat/chatComponents/MessagesList/Message/TextMessage/styles.ts
+++ src/components/Chat/chatComponents/MessagesList/Message/TextMessage/styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const LocationList = styled.ul`
   margin: 0;
   padding: 0px 0px 0px 10px;
+  list-style-type: none;
 `;
 
 export const LocationItem = styled.li``;
