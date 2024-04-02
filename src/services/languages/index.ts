6a086098f6df938e7e14c65741d62b96f7b3580b
Artem-Pavlenko <rozen19942810@gmail.com>
2024-04-02 16:54:50 +0300

    [AP]refactor: localization (fr)

--- src/services/languages/index.ts
+++ src/services/languages/index.ts
@@ -1,3 +1,2 @@
 export * from "./en";
 export * from "./fr";
-export { ua } from "./ua";
--- src/services/localization.ts
+++ src/services/localization.ts
@@ -1,12 +1,11 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import { en, fr, ua } from "services/languages";
+import { en, fr } from "services/languages";
 
 const resources = {
   en,
   fr,
-  ua,
 };
 
 i18n.use(initReactI18next).init({
