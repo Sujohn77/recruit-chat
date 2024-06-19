fcbda998736b18caf945289d9da4b63151e9e444
Artem-Pavlenko <rozen19942810@gmail.com>
2024-06-19 12:12:04 +0300

    [AP]

--- src/components/Layout/SlideUpPanel/index.tsx
+++ src/components/Layout/SlideUpPanel/index.tsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import * as S from "./styles";
 
 interface ISlidePanelProps {
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
   children: React.ReactNode;
+  contentStyle?: CSSProperties;
 }
 
 export const SlideUpPanel: React.FC<ISlidePanelProps> = ({
   isOpen,
   setIsOpen,
   children,
+  contentStyle,
 }) => (
   <S.SlideUpPanelContainer>
     <S.Overlay isOpen={isOpen} onClick={() => setIsOpen(false)} />
-    <S.Panel isOpen={isOpen}>{children}</S.Panel>
+    <S.Panel style={contentStyle} isOpen={isOpen}>
+      {children}
+    </S.Panel>
   </S.SlideUpPanelContainer>
 );
